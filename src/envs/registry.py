5578577bfbd4463e218c2440f3327dadf8dce757
rb876 <riccardo.barbano.19@ucl.ac.uk>
2026-01-14 15:19:53 +0000

    updated seamlessloss

--- src/envs/registry.py
+++ src/envs/registry.py
@@ -2,7 +2,7 @@
 from src.envs.seam_continuity_loss import SeamContinuityLoss
 
 def get_optimality_criterion(name, classifier, mask, **kwargs):
-    if name == "classifier_ce":
+    if name == "classifier_ce_with_alignment_or_cooperation": # seamless alignment or cooperation
         from src.envs.optimality import ClassifierCEWithAlignmentOrCooperation
         return ClassifierCEWithAlignmentOrCooperation(
             classifier=classifier,
